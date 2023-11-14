705535b7a596be7dfd2f19c4300858063b2bb6ab
JooaoVSilva <126405719+JooaoVSilva@users.noreply.github.com>
2023-11-14 20:18:49 -0300

    ScriptAlpaca v2

--- ScriptAlpaca.sh
+++ ScriptAlpaca.sh
@@ -1,28 +1,38 @@
 #!/bin/bash
+
+# Bem-vindo ao Script de instalação da Alpaca Solutions
+echo "Bem-vindo ao Script de instalação da Alpaca Solutions"
+echo "Para começarmos, iremos atualizar o seu apt"
+
 sudo apt update && sudo apt upgrade
+sleep 10
 
+# Agora iremos instalar o pacote de instalações
+read -p "Você possui nosso pacote de instalações? (Sim/Não): " installProject
+if [ "$installProject" = "Sim" ]; then
+  # Atualizando o pacote
+  echo "Atualizando o pacote"
+  git pull
+  sleep 10
+  echo "Pacote atualizado com sucesso"
+  cd ScriptInstalacaoJar/ || exit 1
+else
+  echo "Iremos baixar nosso pacote de instalação usando wget"
+  wget -O ScriptInstalacaoJar.zip https://github.com/Alpaca-Solutions/ScriptInstalacaoJar/archive/main.zip
+  unzip ScriptInstalacaoJar.zip
+  rm ScriptInstalacaoJar.zip
+  cd ScriptInstalacaoJar-main/ || exit 1
+fi
 
-java -version 
-if [ $? = 0 ]; 
-then 
-echo “Cliente possui java instalado!” 
-else 
-echo “Você não possui java instalado!”
-  
-  read instalar
-  if [ \“$intalar\” == \“s\” ];
-then #entao
-sudo apt install openjdk-17-jre -y 
-fi 
+# Para prosseguirmos, iremos iniciar o script do Docker
+echo "Para prosseguirmos, iremos iniciar o script do Docker"
+bash ScriptDocker.sh
 
-fi 
+# Agora, vamos baixar a pasta do projeto usando wget
+echo "Baixando a pasta do projeto usando wget"
+wget -O nome_do_arquivo.zip https://caminho/do/arquivo.zip
+# Substitua "nome_do_arquivo.zip" e "https://caminho/do/arquivo.zip" pelos valores reais
 
-git clone https://github.com/Alpaca-Solutions/Jar-Api-Conectando-ao-Banco.git
+# Adicione qualquer outra lógica necessária para descompactar ou instalar o projeto, se necessário
 
-cd Jar-Api-Conectando-ao-Banco/
-
-git pull/
-
-cd v2-jar-alpaca-solutions/target/
-
-java -jar v2-jar-alpaca-solutions-1.0-jar-with-dependecies.jar
+echo "Script concluído"
