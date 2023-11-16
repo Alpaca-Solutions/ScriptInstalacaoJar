b4576d5d312dd8534c84f294d3d1a9527bc3f995
JooaoVSilva <joao.silva850@sptech.school>
2023-11-16 10:17:50 -0300

    ScriptAlpaca V3.4

--- ScriptAlpaca.sh
+++ ScriptAlpaca.sh
@@ -1,38 +1,36 @@
 #!/bin/bash
 
-# Bem-vindo ao Script de instalação da Alpaca Solutions
-echo "Bem-vindo ao Script de instalação da Alpaca Solutions"
+# Bem vindo ao Script de instalação da Alpaca Solutions
+echo "Bem vindo ao Script de instalação da Alpaca Solutions"
 echo "Para começarmos, iremos atualizar o seu apt"
-
-sudo apt update && sudo apt upgrade
-sleep 10
+sudo apt install 
+sudo apt update
+sleep 5
 
 # Agora iremos instalar o pacote de instalações
-read -p "Você possui nosso pacote de instalações? (Sim/Não): " installProject
+read -p "Você possui nosso pacote de instalações? (Sim/Nao): " installProject
 if [ "$installProject" = "Sim" ]; then
   # Atualizando o pacote
   echo "Atualizando o pacote"
-  git pull
-  sleep 10
-  echo "Pacote atualizado com sucesso"
   cd ScriptInstalacaoJar/ || exit 1
+  git pull
+  sleep 5
+  echo "Pacote atualizado com sucesso"
 else
   echo "Iremos baixar nosso pacote de instalação usando wget"
+  sudo apt intall wget
   wget -O ScriptInstalacaoJar.zip https://github.com/Alpaca-Solutions/ScriptInstalacaoJar/archive/main.zip
+  sudo apt install unzip
   unzip ScriptInstalacaoJar.zip
   rm ScriptInstalacaoJar.zip
   cd ScriptInstalacaoJar-main/ || exit 1
 fi
 
+# Concedendo permissões de execução ao script
+chmod +x ScriptDocker.sh
+
 # Para prosseguirmos, iremos iniciar o script do Docker
 echo "Para prosseguirmos, iremos iniciar o script do Docker"
-bash ScriptDocker.sh
-
-# Agora, vamos baixar a pasta do projeto usando wget
-echo "Baixando a pasta do projeto usando wget"
-wget -O nome_do_arquivo.zip https://caminho/do/arquivo.zip
-# Substitua "nome_do_arquivo.zip" e "https://caminho/do/arquivo.zip" pelos valores reais
-
-# Adicione qualquer outra lógica necessária para descompactar ou instalar o projeto, se necessário
+./ScriptDocker.sh # Executando o script com permissões de execução
 
 echo "Script concluído"
