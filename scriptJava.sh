9deb3d59cd7b4177c8168024acd50d9ec1497ff0
JooaoVSilva <126405719+JooaoVSilva@users.noreply.github.com>
2023-11-27 11:35:12 -0300

    Update scriptJava.sh

--- scriptJava.sh
+++ scriptJava.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # URL do arquivo JAR no GitHub
-jar_url="https://github.com/Alpaca-Solutions/Back-End-Alpaca-Solutions/raw/main/Jar Alpaca Solutions/target/v2-jar-alpaca-solutions-1.0-jar-with-dependencies.jar"
-
+jar_url="https://github.com/Alpaca-Solutions/Jar Alpaca Solutions/target/v2-jar-alpaca-solutions-1.0-jar-with-dependencies.jar"
 # Nome do arquivo JAR após o download
 jar_nome="v2-jar-alpaca-solutions-1.0-jar-with-dependencies.jar"
 
