25b814bc19d6b2c7db9d3c1d3fa4599e1a38ed43
JooaoVSilva <joao.silva850@sptech.school>
2023-12-03 15:25:58 -0300

    script alterado

--- scriptJava.sh
+++ scriptJava.sh
@@ -1,55 +1,67 @@
 #!/bin/bash
 
-# Nome do contêiner Docker
-container_nome="java-app"
-
 # URL do arquivo JAR no GitHub
 jar_url="https://github.com/Alpaca-Solutions/Back-End-Alpaca-Solutions/raw/main/target/sistema-nowl-1.0-jar-with-dependencies.jar"
 
 # Nome do arquivo JAR após o download
 jar_nome="v2-jar-alpaca-solutions-1.0-jar-with-dependencies.jar"
 
+echo "Agora iremos verificar se você já possui o Java instalado, aguarde um instante..."
+sleep 5
+
+if ! command -v java &> /dev/null || ! java --version | grep -q "openjdk 17"; then
+    echo "Você não possui o Java 17 instalado."
+    echo "Confirme se deseja instalar o Java 17 (S/N)?"
+    read inst
+    if [ "$inst" == "S" ]; then
+        echo "Ok! Você escolheu instalar o Java 17."
+        echo "Adicionando o repositório..."
+        sleep 7
+        sudo add-apt-repository ppa:linuxuprising/java -y
+        clear
+        echo "Atualizando os pacotes... Quase acabando."
+        sleep 7
+        sudo apt update -y
+
+        # Instalação do Java 17
+        echo "Preparando para instalar a versão 17 do Java. Lembre-se de confirmar a instalação quando necessário!"
+        sudo apt-get install openjdk-17-jdk -y
+        clear
+        echo "Java 17 instalado com sucesso!"
+        echo "Vamos atualizar os pacotes..."
+        sudo apt update && sudo apt upgrade -y
+    else
+        echo "Você optou por não instalar o Java 17 no momento."
+    fi
+else
+    echo "Você já possui o Java 17 instalado!"
+fi
+
 # Verificar se o arquivo JAR já existe
 if [ ! -f "$jar_nome" ]; then
     echo "Baixando o arquivo JAR..."
     # Instale o wget se não estiver instalado
-    sudo apt-get install wget -y
+    sudo apt install wget -y
     # Baixar o arquivo JAR usando wget
-    if ! wget -q "$jar_url" -O "$jar_nome"; then
+    wget "$jar_url" -O "$jar_nome"
+
+    # Verificar se o download foi bem-sucedido
+    if [ $? -eq 0 ]; then
+        echo "Download do arquivo JAR concluído com sucesso."
+    else
         echo "Erro ao baixar o arquivo JAR."
         exit 1
     fi
-    echo "Download do arquivo JAR concluído com sucesso."
 else
     echo "Arquivo JAR já existe. Pulando o download."
 fi
 
-# Verificar se o Docker está instalado
-if ! command -v docker &> /dev/null; then
-    echo "Docker não encontrado. Certifique-se de que o Docker esteja instalado e executando."
-    exit 1
-fi
+# Executar o arquivo JAR
+java -jar "$jar_nome"
 
-# Criar o arquivo Dockerfile
-echo "Criando o arquivo Dockerfile..."
-cat <<EOF > Dockerfile
-FROM openjdk:17
-COPY $jar_nome /app.jar
-CMD java -jar /app.jar
-EOF
-
-# Construir a imagem Docker
-echo "Construindo a imagem Docker..."
-if ! sudo docker build -t "$container_nome" .; then
-    echo "Erro ao construir a imagem Docker."
-    exit 1
+# Verificar se a execução foi bem-sucedida
+if [ $? -eq 0 ]; then
+    echo "Execução do arquivo JAR bem-sucedida."
+else
+    echo "Erro ao executar o arquivo JAR."
 fi
-echo "Construção da imagem Docker concluída com sucesso."
-
-# Executar o contêiner Docker
-echo "Executando o contêiner Docker..."
-if ! sudo docker run --rm -it "$container_nome"; then
-    echo "Erro ao executar o contêiner Docker."
-    exit 1
-fi
-echo "Execução do contêiner Docker bem-sucedida."
